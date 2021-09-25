17175ef0f14e71bd735834ec2484adc78fa49a1d
borshon420 <borshonarafat420@gmail.com>
2021-09-25 15:04:14 +0600

    cart calcultation

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,17 +7,17 @@ const Cart = (props) => {
         total = total + product.price;
     }
     const shipping = 15;
-    const tax = (total + shipping) * 10
-    const grandTotal = total + shipping + tax;
+    // const tax = (total + shipping) * 10
+    const grandTotal = total + shipping ;
     return (
         <div>
             <h3>Order Summary</h3>
                 <h3>Items ordered: {props.cart.length}</h3>
                 <br />
                 <p>Shipping: {shipping}</p>
-                <p>Tax: {tax.toFixed(2)}</p>
-                <p>Grand Total: {grandTotal.toFixed(2)}</p>
+                {/* <p>Tax: {tax.toFixed(2)}</p> */}
                 <p>Total: {total.toFixed(2)}</p>
+                <p>Grand Total: {grandTotal.toFixed(2)}</p>
         </div>
     );
 };
